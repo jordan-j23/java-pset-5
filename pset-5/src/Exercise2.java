39d5325622061f38a1d13f41b4890753bc795ac6
jordan-j23 <jordanejenk@gmail.com>
2020-10-11 21:38:28 -0400

    final commit

---
+++ pset-5/src/Exercise2.java
@@ -0,0 +1,16 @@
+mport java.util.Scanner;
+
+public class Excercise2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+        if (number % 2 == 0){
+            System.out.println("\nEven.");
+        } else {
+            System.out.println("\nOdd.");
+        }
+        input.close();
+    }
+}
